867845773249a849865f243029c158c1347e45a2
kumabe <kumabe@catchup.co.jp>
2025-09-11 00:46:10 +0900

    13完了

--- laravel9-build-template/src/LaravelTestProject/app/Http/Controllers/Auth/ResetPasswordController.php
+++ laravel9-build-template/src/LaravelTestProject/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Routing\Route;
 
 class ResetPasswordController extends Controller
 {
@@ -25,5 +27,5 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = RouteServiceProvider::HOME;
 }
--- laravel9-build-template/src/LaravelTestProject/resources/lang/ja/validation.php
+++ laravel9-build-template/src/LaravelTestProject/resources/lang/ja/validation.php
@@ -155,6 +155,9 @@ return [
 
     'attributes' => [
         'title' => 'フォルダ名',
+        'email' => 'メールアドレス',
+        'password' => 'パスワード',
+        'token' => 'トークン',
     ],
 
 ];
